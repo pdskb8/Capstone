55318576bbcfe3e449795e766b1902ab444d5c52
Paul Shannon <paulshannon36@gmail.com>
2019-08-28 15:31:22 -0500

    Update dependecy of Main.js.

--- index.js
+++ index.js
@@ -1,151 +1,226 @@
-import Navigation from './components/Navigation';
-import Header from './components/Header';
-import Main from './components/Main';
-import Footer from './components/Footer';
-import * from './components/comicInterface.js';
-import Login from './components/Login.js';
+import Navigation from "./components/Navigation";
+import Header from "./components/Header";
+import Main from "./components/Main";
+import Footer from "./components/Footer";
+// import * from './components/comicInterface.js';
+// import Login from './components/Login.js';
 
-import Navigo from 'navigo';
+import Navigo from "navigo";
 
 const router = new Navigo(location.origin);
 
-
 // console.log(Navigation, Header, Main, Footer);
 
-
-//  store Object
-const store = {
-
-    'collection': {
-        'links': {
-            'primary': [ 'Collection', 'Pull List', 'Comic Shops', 'Comic News' ],
-        },
-        'title': 'Profile',
-        'page': `
-
-      `
-    },
-
-    'pull list': {
-        'links': {
-            'primary': [ 'Collection', 'Pull List', 'Comic Shops', 'Comic News' ],
-        },
-        'title': 'Pull List',
-        'page': `
-
-      `
-    },
-
-    'comic shops': {
-        'links': {
-            'primary': [ 'Collection', 'Pull List', 'Comic Shops', 'Comic News' ],
-        },
-        'title': 'Comic Shop List',
-        'page': `
-
-      `
-    },
-
-    'comic news': {
-        'links': {
-            'primary': [ 'Collection', 'Pull List', 'Comic Shops', 'Comic News' ],
-        },
-        'title': 'Comic News',
-        'page': `
-
-      `
-    },
-
-};
-
-const isLoggedIn = false;
-
 let collection = [];
+let pullList = [];
 
-class ComicBook  {
-  constructor(title, issue, variant, printing) {
-    this.title = title,
-    this.issue = issue,
-    this.variant = variant,
-    this.printing = printing
+class ComicBook {
+  constructor(title, barcode) {
+    (this.title = barcode.sub),
+      (this.issue = issue),
+      (this.variant = variant),
+      (this.printing = printing),
+      (this.id = this.title + barcode);
   }
 }
-
-class newUser  {
-  constructor(username, email, password) {
-    
-    this.username = "Nobody",
-    this.email =  "imaginethis25@yahoo.com",
-    this.password = "nothing2019"
-  }
-}
-
-const users = [
-  {
-    "username": "Nobody",
-    "email": "imaginethis25@yahoo.com",
-    "password": "nothing2019"
-  },
-];
 
 function showCollection() {
-  let collectionHTML = `<ul class="item">`;
+  let collectionHTML = `<ul>`;
 
-  collection.forEach( (item) => {
-    
-    collectionHTML +=`<li>${item[property].value}</li>`;
-
+  collection.forEach(item => {
+    collectionHTML += `<li>${item[property].value}</li>`;
   });
   collectionHTML += `</ul`;
+  document.querySelector("listView").innerHTML = collectionHTML;
 }
 
-
-// Add New User to Account List
-const newUserEntry = document.querySelector('#newCollector form');
-
-newUserEntry.addEventListener('submit', event => {
-
-  event.preventDefault();
-  
-
-  const data = event.target.elements;
-  const collector = newUser(data[0],data[1],data[2]);
-
-
-
-})
-
-function showCollectionBy(collection, usrQuery){
+function showCollectionBy(collection, usrQuery) {
   const result = collection.filter(comic => comic[item].value == usrQuery);
-  let listHTML = `<ul class="item">`;
+  let listHTML = `<ul>`;
   result.forEach(item => {
     listHTML += `<li>${item}</li>`;
   });
   listHTML += `</ul>`;
-  return listHTML;
-
+  document.querySelector("listView").innerHTML = listHTML;
 }
 
+// Login
+const isLoggedIn = false;
+//   const users = JSON.parse(userAccounts);
 
-const render = (store) => {
+// function Login(){
+//   const loginForm = document.querySelector('#login');
+
+//   loginForm.addEventListener('submit', (event) => {
+//       const data = event.target.elements;
+
+//       if(data[0].target in users[username] && data[1].target in users[email]){
+//           return isLoggedIn = true;
+//       }
+//   });
+// }
+
+// Login();
+
+// // Add New User to Account List
+// const newUserEntry = document.querySelector('#newCollector form');
+
+// newUserEntry.addEventListener('submit', event => {
+
+//   event.preventDefault();
+
+//   const data = event.target.elements;
+//   const collector = newUser(data[0],data[1],data[2]);
+
+// });
+
+// // New Users Sign Up
+
+// class newUser  {
+//   constructor(username, email, password) {
+
+//     this.username = "Nobody",
+//     this.email =  "imaginethis25@yahoo.com",
+//     this.password = "nothing2019"
+//   }
+// }
+
+// Render Function
+const render = state => {
   // We use function invocation that actually runs the fxns. and then 'returns' the markup ao that
   //
-  document.querySelector('#root').innerHTML = `
-${Navigation(store)}
-${Header(store)}
-${Main(store)}
-${Footer(store)}`;
-
+  document.querySelector("#root").innerHTML = `
+${Navigation(state)}
+${Header(state)}
+${Main(state)}
+${Footer(state)}`;
 
   router.updatePageLinks();
 };
 
-render(store.home);
+
+//  store Object
+const store = {
+  "profile": {
+    "links": {
+      "primary": ["Collection", "Pull List", "Comic Shops", "Comic News"]
+    },
+    "title": `Profile of Guest`,
+    "page": `
+      <div id="usr-profile">
+      <h2>Your Profile</h2>
+      <img src="" alt="profile avatar" id="avatar">
+      <ul class="usr-info">
+        <li>Profile: Guest </li>
+        <li>Email: pds@pdskb8.com</li>
+        <li>Number of comics: ${collection.length} </li>
+        
+    </ul>
+    <input type="search" name="comic_lookup" class="comic_lookup">
+    <input type="button" name="lookup-btn" class="lookup-btn">
+    <input type="button" class="display-collection" >
+    <input type="button" class="clear-view">
+    <div class="comic-btn-operators">
+        <input type="button" class="addComic">
+        <input type="button" class="editComic">
+        <input type="button" class="deleteComic">
+    </div>
+    
+  
+    </div>
+    <div id="form" id="collection-form">
+        <form action="" method="GET" >
+      
+            <input type="text" name="comic_title" id="comic_title" >
+            <input type="text"  name="printing" id="printing">
+            <input type="text"  name="issue"  id="issue">
+            <input type="text"  name="variant" id="variant">
+            <input type="hidden"  name="item_num" id="item_num">
+            <input type="submit" >
+            <input type="reset" >
+          </form>
+    </div>
+    <div class="view-list">
+      <h2>Your Collection</h2>
+    </div>
+    `
+  },
+
+  "pull list": {
+    "links": {
+      "primary": ["Collection", "Pull List", "Comic Shops", "Comic News"]
+    },
+    "title": `Pull List of Guest`,
+    "page": `
+      <div id="usr-profile">
+      <h2>Your Profile</h2>
+      <img src="" alt="profile avatar" id="avatar">
+      <ul class="usr-info">
+        <li>Profile: Guest </li>
+        <li>Email: pds@pdskb8.com</li>
+        <li>Number of comics: ${collection.length} </li>
+        
+    </ul>
+    <input type="search" name="comic_lookup" class="comic_lookup">
+    <input type="button" name="lookup-btn" class="lookup-btn">
+    <input type="button" class="display-collection" >
+    <input type="button" class="clear-view">
+    <div class="comic-btn-operators">
+        <input type="button" class="addComic">
+        <input type="button" class="editComic">
+        <input type="button" class="deleteComic">
+    </div>
+      <form >
+      <input type="search" class="lookUp">
+      <input type="text" id="title"> 
+      <input type="number" id="barcode">
+      <input type="number" id="quantity">
+      <input type="button" class="add">
+      <input type="button" class="edit">
+      <input type="button" class="delete">
+      <!-- <button class="delete"></button>
+      <button class="edit"></button>
+      <button class="add"></button>
+  -->
+  </form>
+  <div class="view-list">
+  <h2>Your Your Pull List</h2>
+</div>
+
+    `
+  },
+
+  "comic shops": {
+    "links": {
+      "primary": ["Collection", "Pull List", "Comic Shops", "Comic News"]
+    },
+    "title": `Comic Shops near Guest`,
+    "page": `
+      <div class="view-list">
+  <h2>Your Your Pull List</h2>
+</div>
+    `
+  },
+
+  "comic news": {
+    "links": {
+      "primary": ["Collection", "Pull List", "Comic Shops", "Comic News"]
+    },
+    "title": "Comic News",
+    "page": `
+      <div class="comicbook-news">
+      <h2>What's Hot: </h2>
+    </div>
+    `
+  }
+};
 
 
 router
-  .on(':view', (params) => {
-      console.log(params);
-      render(store[params.view]);
+  .on(":view", params => {
+    console.log(params);
+    render(store[params.view]);
   })
   .resolve();
+
+render(store.profile);
